257c25229cab3e84f841f3678f80d97bf56738eb
Ednilson Albuquerque <ednilson.albuquerque@conexasaude.com.br>
2021-08-04 03:05:01 -0400

    chore: add query client provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import Pokemons from './components/Pokemons/intex';
+
+const queryClient = new QueryClient();
 
 function App() {
+  const [showList, setShowList] = useState(false)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <button onClick={() => setShowList(true)}>Show Pokemon List</button>
+      {
+        showList && 
+        <Pokemons onClose={() => setShowList(false)} />
+      }
+    </QueryClientProvider>
   );
 }
 
