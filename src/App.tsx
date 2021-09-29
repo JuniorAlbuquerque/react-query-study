36da8144648318e7b8b283fe7c1885e97e34a9d1
Ednilson Albuquerque <ednilson.albuquerque@conexasaude.com.br>
2021-09-29 10:38:08 -0400

    teste 3 merge

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <div className="container">
-        <h5>Pokedex with React Query - v2</h5>
+        <h2>Pokedex with React Query - v3</h2>
         <p style={{ marginBottom: 34 }}>Developed by: Jnr</p>
         <Pokemons />
       </div>
