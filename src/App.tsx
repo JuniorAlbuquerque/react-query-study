72e77a1a1f18fed6a22e4608e822eb9e8be7accb
Ednilson Albuquerque <ednilson.albuquerque@conexasaude.com.br>
2021-09-29 22:24:56 -0400

    chore: change app

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <div className="container">
-        <h1>Pokedex with React Query - v1</h1>
-        <p style={{ marginBottom: 34 }}>Developed by: Jnr</p>
+        <h4>Pokedex with React Query - (Teste) - v1</h4>
+        <p style={{ marginBottom: 38 }}>Developed by: Jnr Albuquerque</p>
         <Pokemons />
       </div>
     </QueryClientProvider>
